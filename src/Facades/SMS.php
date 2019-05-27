680de9d16fa9d33ff3f94a4534caa6e49ad5e9d4
ALG <513051043@qq.com>
2019-05-27 15:41:43 +0800

    门面添加方法注释

--- src/Facades/SMS.php
+++ src/Facades/SMS.php
@@ -18,6 +18,7 @@ use Alg\SMS\Contract\SMS as abstractSMS;
  * @author   ALG <513051043@qq.com>
  * @license  四川猪太帅科技公司 http://www.51zts.com
  * @link     接口文档链接
+ * @method static mixed getRandCode()
  */
 class SMS
 {
